c958469b8c25067696c6ec02716bb2360aaed62d
Chris <cldarling@gmail.com>
2025-04-18 13:48:30 -0400

    what to do next

--- index.js
+++ index.js
@@ -2,33 +2,7 @@
 // http://www.omdbapi.com/?apikey=[yourkey]&
 // http://img.omdbapi.com/?apikey=[yourkey]&
 
-const inputField = document.getElementById("searchInput");
 
+res = fetch("https://omdbapi.com/?apikey=9afe3bdf&s=fast")
+console.log(res);
 
-// load movies from API
-async function loadMovies(searchTerm){
-    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=9afe3bdf`;
-    const res = await fetch(`${URL}`);
-    const data = await res.json();
-    console.log(searchTerm);
-    if(data.Response == "True") displayMovieList(data.Search);
-}
-
-loadMovies()
-
-function displayMovieList(data) {
-    return `<div class="movie__results">
-        <div class="result__container">
-          <figure class="poster__img">
-            <img
-              src="${data.Poster}"
-              alt=""
-            />
-          </figure>
-          <div>
-            <h3>${data.Title}</h3>
-            <p>${data.Year}</p>
-          </div>
-        </div>
-      </div>`
-}
